419224c273a71ce2fa0c34f9b5ee8cc8c74535c9
leducthanhig <leducthanh.ig@gmail.com>
2025-04-30 19:14:45 +0700

    feat: implement `Service` class

--- src/models/Service.h
+++ src/models/Service.h
@@ -1,8 +1,37 @@
 #ifndef SERVICE_H
 #define SERVICE_H
 
-class Service {
+#include <string>
+#include "Department.h"
 
+class Service {
+private:
+    std::string _serviceId;
+    std::string _name;
+    double _cost;
+    std::string _status;
+    std::string _description;
+    Department* _department;
+
+public:
+    Service(const std::string& id, const std::string& name, double cost, const std::string& status,
+        const std::string& description, Department* department);
+
+public:
+    std::string id() const;
+    std::string name() const;
+    double cost() const;
+    std::string status() const;
+    std::string description() const;
+    Department* department() const;
+
+public:
+    void setId(const std::string& id);
+    void setName(const std::string& name);
+    void setCost(double cost);
+    void setStatus(const std::string& status);
+    void setDescription(const std::string& description);
+    void setDepartment(Department* department);
 };
 
 #endif
