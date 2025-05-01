4de9cd4c4f15d66627db3d3d09af12584dc1c0fc
Taylor3691 <hoangngoc3691@gmail.com>
2025-05-01 22:59:07 +0700

    feat: Declare attribute and func MedicineUsage class

---
+++ src/models/MedicineUsage.h
@@ -0,0 +1,23 @@
+#ifndef MEDICINEUSAGE_H_
+#define MEDICINEUSAGE_H_
+#include "Medicine.h"
+class MedicineUsage {
+private:
+    Medicine _medicine;
+    int _dosage;
+    std::pair<int, std::string> _frequency;
+    std::string _instructions;
+
+public:
+    MedicineUsage();
+    MedicineUsage(const Medicine& medicine, int dosage, std::pair<int, std::string> frequency, std::string instruction);
+    ~MedicineUsage();
+
+public:
+    std::string getMedicineInfo();
+    std::string getInstructions();
+    void setMedicine(Medicine& medicine);
+    void setInstructions(std::string instruction);
+};
+
+#endif // !MEDICINEUSAGE_H_
