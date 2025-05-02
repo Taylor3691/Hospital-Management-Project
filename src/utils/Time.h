029f3c51439ad2c537bb39a9e7c9c25bfa5f1a68
leducthanhig <leducthanh.ig@gmail.com>
2025-05-03 00:47:52 +0700

    chore(Time): add get/setters and missing operators

--- src/utils/Time.h
+++ src/utils/Time.h
@@ -12,24 +12,42 @@ private:
     int _m;
     int _s;
 
-    void normalize();
-
 public:
     Time();
     Time(int s);
     Time(int m, int s);
     Time(int h, int m, int s);
     Time(const std::string& str);
-    
+
+public:
+    int hour() const;
+    int minute() const;
+    int second() const;
+
+public:
+    void setHour(int h);
+    void setMinute(int m);
+    void setSecond(int s);
+
+public:
     static std::string toString(int s);
     static int toSec(const std::string& str);
     static Time getCurrentTime();
 
+private:
+    void normalize();
+
+public:
     std::string toString() const;
     int toSec() const;
+
+public:
     bool operator<(const Time& t) const;
     bool operator<=(const Time& t) const;
+    bool operator>(const Time& t) const;
+    bool operator>=(const Time& t) const;
     bool operator==(const Time& t) const;
+    bool operator!=(const Time& t) const;
     int operator-(const Time& t) const;
 };
 
