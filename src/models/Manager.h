63c6ef464b226e3e01c01deee472c3901cd24b20
leducthanhig <leducthanh.ig@gmail.com>
2025-05-04 22:58:46 +0700

    feat: add `Manager` class

---
+++ src/models/Manager.h
@@ -0,0 +1,44 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <vector>
+#include <memory>
+#include <string>
+#include "../utils/QueryBuilder.h"
+
+template<typename T>
+class Manager {
+public:
+    virtual void add(std::vector<std::unique_ptr<T>>& entities, std::unique_ptr<T> entity);
+    void removeById(std::vector<std::unique_ptr<T>>& entities, const std::string& id);
+    void removeByName(std::vector<std::unique_ptr<T>>& entities, const std::string& name);
+    T* findById(std::vector<std::unique_ptr<T>>& entities, const std::string& id);
+    std::vector<T*> findByName(std::vector<std::unique_ptr<T>>& entities, const std::string& name);
+};
+
+#endif
+
+template<typename T>
+void Manager<T>::add(std::vector<std::unique_ptr<T>>& entities, std::unique_ptr<T> entity) {
+    entities.push_back(std::move(entity));
+}
+
+template<typename T>
+void Manager<T>::removeById(std::vector<std::unique_ptr<T>>& entities, const std::string& id) {
+    from(entities).where(makeGetter(&T::id), id).deleteOne();
+}
+
+template<typename T>
+void Manager<T>::removeByName(std::vector<std::unique_ptr<T>>& entities, const std::string& name) {
+    from(entities).where(makeGetter(&T::name), name).deleteMany();
+}
+
+template<typename T>
+T* Manager<T>::findById(std::vector<std::unique_ptr<T>>& entities, const std::string& id) {
+    return from(entities).where(makeGetter(&T::id), id).findOne();;
+}
+
+template<typename T>
+std::vector<T*> Manager<T>::findByName(std::vector<std::unique_ptr<T>>& entities, const std::string& name) {
+    return from(entities).where(makeGetter(&T::name), name).find();;
+}
