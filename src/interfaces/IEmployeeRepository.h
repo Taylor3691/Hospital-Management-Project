957f0eb3fd30fc896958d1b754f7f99e46605b57
leducthanhig <leducthanh.ig@gmail.com>
2025-05-13 11:04:12 +0700

    chore(IEmployeeRepository): remove `employees` pure virtual getter

--- src/interfaces/IEmployeeRepository.h
+++ src/interfaces/IEmployeeRepository.h
@@ -1,16 +1,12 @@
 #ifndef IEMPLOYEEREPOSITORY
 #define IEMPLOYEEREPOSITORY
 
-#include <vector>
 #include "IRepository.h"
 #include "../models/Employee.h"
 
 class IEmployeeRepository : public IRepository<Employee> {
 public:
     virtual ~IEmployeeRepository() = default;
-
-public:
-    virtual std::vector<const Employee*> employees() const = 0;
 };
 
 #endif // !IEMPLOYEEREPOSITORY
