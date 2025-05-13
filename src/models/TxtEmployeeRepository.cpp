88c7b1daef2f8c3bbec73ea7863c6f1a4cafb81c
leducthanhig <leducthanh.ig@gmail.com>
2025-05-13 11:11:55 +0700

    refactor(TxtEmployeeRepo): add helpers for `save` and `load`

--- src/models/TxtEmployeeRepository.cpp
+++ src/models/TxtEmployeeRepository.cpp
@@ -1,137 +1,121 @@
 #include "TxtEmployeeRepository.h"
 
 TxtEmployeeRepository::TxtEmployeeRepository()
-    : _doctorFile("doctors.txt"), _nurseFile("nurses.txt"),
-    _receptionistFile("receptionist.txt") {}
+    : TxtEmployeeRepository("doctors.txt",
+        "nurses.txt", "receptionists.txt") {}
 
 TxtEmployeeRepository::TxtEmployeeRepository(
     const std::string& doctorFile,
     const std::string& nurseFile,
-    const std::string& receptionistFile
+    const std::string& receptionistFile,
+    char delim
 ) : _doctorFile(doctorFile), _nurseFile(nurseFile),
-    _receptionistFile(receptionistFile) {}
+    _receptionistFile(receptionistFile), _delim(delim) {}
+
+void TxtEmployeeRepository::serialize(
+    std::ofstream& file, const Employee* employee
+) {
+    file << employee->id() << "|"
+        << employee->name() << "|"
+        << employee->dob() << "|"
+        << employee->gender() << "|"
+        << employee->phone() << "|"
+        << employee->address() << "|"
+        << employee->education() << "|"
+        << std::to_string(employee->baseSalary());
+}
+
+void TxtEmployeeRepository::serialize(
+    std::ofstream& file, const Doctor* doctor
+) {
+    serialize(file, static_cast<const Employee*>(doctor));
+
+    file << "|" << doctor->specialty() << "|"
+        << doctor->license();
+}
+
+void TxtEmployeeRepository::serialize(
+    std::ofstream& file, const Nurse* nurse
+) {
+    serialize(file, static_cast<const Employee*>(nurse));
+
+    file << "|" << nurse->duty();
+}
+
+void TxtEmployeeRepository::serialize(
+    std::ofstream& file, const Receptionist* receptionist
+) {
+    serialize(file, static_cast<const Employee*>(receptionist));
+
+    file << "|" << std::to_string(receptionist->subsidies()) << "|"
+        << std::to_string(receptionist->workingDays());
+}
+
+void TxtEmployeeRepository::deserialize(
+    std::ifstream& file, Employee* employee, std::string& buffer
+) {
+    employee->setId(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setName(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setDob(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setGender(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setPhone(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setAddress(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setEducation(buffer);
+
+    std::getline(file, buffer, '|');
+    employee->setBaseSalary(stod(buffer));
+}
+
+void TxtEmployeeRepository::deserialize(
+    std::ifstream& file, Doctor* doctor, std::string& buffer
+) {
+    deserialize(file, static_cast<Employee*>(doctor), buffer);
+
+    std::getline(file, buffer, '|');
+    doctor->setSpecialty(buffer);
+
+    std::getline(file, buffer);
+    doctor->setLicense(buffer);
+}
+
+void TxtEmployeeRepository::deserialize(
+    std::ifstream& file, Nurse* nurse, std::string& buffer
+) {
+    deserialize(file, static_cast<Employee*>(nurse), buffer);
+
+    std::getline(file, buffer);
+    nurse->setDuty(buffer);
+}
+
+void TxtEmployeeRepository::deserialize(
+    std::ifstream& file, Receptionist* receptionist, std::string& buffer
+) {
+    deserialize(file, static_cast<Employee*>(receptionist), buffer);
+
+    std::getline(file, buffer, '|');
+    receptionist->setSubsidies(stod(buffer));
+
+    std::getline(file, buffer);
+    receptionist->setWorkingDays(stoi(buffer));
+}
 
 void TxtEmployeeRepository::load() {
     _employees.clear();
-
-    std::string buffer;
-
-    std::ifstream fd(_doctorFile);
-    if (!fd.is_open()) {
-        throw std::runtime_error("Cannot open doctor file for reading");
-    }
-
-    while (std::getline(fd, buffer, '|')) {
-        auto doctor = std::make_unique<Doctor>();
-        doctor->setId(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setName(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setDob(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setGender(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setPhone(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setAddress(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setEducation(buffer);
-
-        std::getline(fd, buffer, '|');
-        doctor->setBaseSalary(stod(buffer));
-
-        std::getline(fd, buffer, '|');
-        doctor->setSpecialty(buffer);
-
-        std::getline(fd, buffer);
-        doctor->setLicense(buffer);
-
-        _employees.push_back(std::move(doctor));
-    }
-    fd.close();
-
-    std::ifstream fn(_nurseFile);
-    if (!fn.is_open()) {
-        throw std::runtime_error("Cannot open nurse file for reading");
-    }
-
-    while (std::getline(fn, buffer, '|')) {
-        auto nurse = std::make_unique<Nurse>();
-        nurse->setId(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setName(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setDob(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setGender(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setPhone(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setAddress(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setEducation(buffer);
-
-        std::getline(fn, buffer, '|');
-        nurse->setBaseSalary(stod(buffer));
-
-        std::getline(fn, buffer);
-        nurse->setDuty(buffer);
-
-        _employees.push_back(std::move(nurse));
-    }
-    fn.close();
-
-    std::ifstream fr(_receptionistFile);
-    if (!fr.is_open()) {
-        throw std::runtime_error("Cannot open receptionist file for reading");
-    }
-
-    while (std::getline(fr, buffer, '|')) {
-        auto receptionist = std::make_unique<Receptionist>();
-        receptionist->setId(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setName(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setDob(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setGender(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setPhone(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setAddress(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setEducation(buffer);
-
-        std::getline(fr, buffer, '|');
-        receptionist->setBaseSalary(stod(buffer));
-
-        std::getline(fr, buffer, '|');
-        receptionist->setSubsidies(stod(buffer));
-
-        std::getline(fr, buffer);
-        receptionist->setWorkingDays(stoi(buffer));
-
-        _employees.push_back(std::move(receptionist));
-    }
-    fr.close();
+    load<Doctor>(_doctorFile, "Cannot open doctor file for reading");
+    load<Nurse>(_nurseFile, "Cannot open nurse file for reading");
+    load<Receptionist>(_receptionistFile, "Cannot open receptionist file for reading");
 }
 
 void TxtEmployeeRepository::save() const {
@@ -155,39 +139,16 @@ void TxtEmployeeRepository::save() const {
 
     for (const auto& employee : _employees) {
         if (auto doctor = dynamic_cast<Doctor*>(employee.get())) {
-            fd << doctor->id().c_str() << "|"
-                << doctor->name().c_str() << "|"
-                << doctor->dob() << "|"
-                << doctor->gender().c_str() << "|"
-                << doctor->phone().c_str() << "|"
-                << doctor->address().c_str() << "|"
-                << doctor->education().c_str() << "|"
-                << std::to_string(doctor->baseSalary()).c_str() << "|"
-                << doctor->specialty().c_str() << "|"
-                << doctor->license().c_str() << "\n";
+            serialize(fd, doctor);
+            fd << "\n";
         }
         else if (auto nurse = dynamic_cast<Nurse*>(employee.get())) {
-            fn << nurse->id().c_str() << "|"
-                << nurse->name().c_str() << "|"
-                << nurse->dob() << "|"
-                << nurse->gender().c_str() << "|"
-                << nurse->phone().c_str() << "|"
-                << nurse->address().c_str() << "|"
-                << nurse->education().c_str() << "|"
-                << std::to_string(nurse->baseSalary()).c_str() << "|"
-                << nurse->duty().c_str() << "\n";
+            serialize(fn, nurse);
+            fn << "\n";
         }
         else if (auto receptionist = dynamic_cast<Receptionist*>(employee.get())) {
-            fr << receptionist->id().c_str() << "|"
-                << receptionist->name().c_str() << "|"
-                << receptionist->dob() << "|"
-                << receptionist->gender().c_str() << "|"
-                << receptionist->phone().c_str() << "|"
-                << receptionist->address().c_str() << "|"
-                << receptionist->education().c_str() << "|"
-                << std::to_string(receptionist->baseSalary()).c_str() << "|"
-                << std::to_string(receptionist->subsidies()).c_str() << "|"
-                << std::to_string(receptionist->workingDays()).c_str() << "\n";
+            serialize(fr, receptionist);
+            fr << "\n";
         }
     }
 
@@ -207,14 +168,15 @@ void TxtEmployeeRepository::removeById(const std::string& id) {
 }
 
 void TxtEmployeeRepository::update(const Employee& employee) {
-    auto ptr = from(_employees).where(&Employee::id, employee.id()).findOne();
+    auto ptr = from(_employees)
+        .where(&Employee::id, employee.id()).findOne();
     if (ptr) {
         *ptr = employee;
     }
     save();
 }
 
-std::vector<const Employee*> TxtEmployeeRepository::employees() const {
+std::vector<const Employee*> TxtEmployeeRepository::data() const {
     std::vector<const Employee*> result;
     result.reserve(_employees.size());
     for (const auto& emp : _employees) {
--- src/models/TxtEmployeeRepository.h
+++ src/models/TxtEmployeeRepository.h
@@ -2,11 +2,14 @@
 #define TXTEMPLOYEEREPOSITORY_H
 
 #include <fstream>
+#include <typeinfo>
 #include "../interfaces/IEmployeeRepository.h"
 #include "../utils/QueryBuilder.h"
 #include "Doctor.h"
 #include "Nurse.h"
 #include "Receptionist.h"
+
+#define DEFAULT_DELIM '|'
 
 class TxtEmployeeRepository : public IEmployeeRepository {
 private:
@@ -14,11 +17,26 @@ private:
     std::string _nurseFile;
     std::string _receptionistFile;
     std::vector<std::unique_ptr<Employee>> _employees;
+    char _delim;
 
 public:
     TxtEmployeeRepository();
-    TxtEmployeeRepository(const std::string& doctorFile,
-        const std::string& nurseFile, const std::string& receptionistFile);
+    TxtEmployeeRepository(const std::string& doctorFile, const std::string& nurseFile,
+        const std::string& receptionistFile, char delim = DEFAULT_DELIM);
+
+private:
+    static void serialize(std::ofstream& file, const Employee* employee);
+    static void serialize(std::ofstream& file, const Doctor* doctor);
+    static void serialize(std::ofstream& file, const Nurse* nurse);
+    static void serialize(std::ofstream& file, const Receptionist* receptionist);
+    static void deserialize(std::ifstream& file, Employee* employee, std::string& buffer);
+    static void deserialize(std::ifstream& file, Doctor* doctor, std::string& buffer);
+    static void deserialize(std::ifstream& file, Nurse* nurse, std::string& buffer);
+    static void deserialize(std::ifstream& file, Receptionist* receptionist, std::string& buffer);
+
+private:
+    template<typename T>
+    void load(const std::string& fileName, const char* errorMsg);
 
 public:
     void load() override;
@@ -26,7 +44,24 @@ public:
     void add(std::unique_ptr<Employee> employee) override;
     void removeById(const std::string& id) override;
     void update(const Employee& employee) override;
-    std::vector<const Employee*> employees() const override;
+    std::vector<const Employee*> data() const override;
 };
 
 #endif // !TXTEMPLOYEEREPOSITORY_H
+
+template<typename T>
+inline void TxtEmployeeRepository::load(const std::string& fileName, const char* errorMsg) {
+    std::string buffer;
+
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        throw std::runtime_error(errorMsg);
+    }
+
+    while (std::getline(file, buffer, _delim)) {
+        auto employee = std::make_unique<T>();
+        deserialize(file, employee.get(), buffer);
+        _employees.push_back(std::move(employee));
+    }
+    file.close();
+}
