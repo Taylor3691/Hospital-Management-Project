d25ca1600c5492fe3d58c73fe44e2c3e796c91f4
leducthanhig <leducthanh.ig@gmail.com>
2025-05-13 11:08:10 +0700

    refactor(EmployeeQuery): update `employees` to `data` for matching parent method name

--- src/models/EmployeeQuery.cpp
+++ src/models/EmployeeQuery.cpp
@@ -4,33 +4,33 @@ EmployeeQuery::EmployeeQuery(IEmployeeRepository* repo)
     : _repo(repo) {}
 
 const Employee* EmployeeQuery::findById(const std::string& id) const {
-    auto employees = _repo->employees();
+    auto employees = _repo->data();
     return from(employees).where(&Employee::id, id).findOne();
 }
 
 std::vector<const Employee*> EmployeeQuery::findAll() const {
-    auto employees = _repo->employees();
+    auto employees = _repo->data();
     return from(employees).find();
 }
 
 std::vector<const Employee*> EmployeeQuery::findByName(
     const std::string& name
 ) const {
-    auto employees = _repo->employees();
+    auto employees = _repo->data();
     return from(employees).where(&Employee::name, name).find();
 }
 
 std::vector<const Employee*> EmployeeQuery::findByEducation(
     const std::string& education
 ) const {
-    auto employees = _repo->employees();
+    auto employees = _repo->data();
     return from(employees).where(&Employee::education, education).find();
 }
 
 std::vector<const Employee*> EmployeeQuery::findByType(
     const std::string& type
 ) const {
-    auto employees = _repo->employees();
+    auto employees = _repo->data();
     return from(employees)
         .where([](const auto& e) { return std::string(typeid(e).name()); }, type)
         .find();
@@ -39,6 +39,6 @@ std::vector<const Employee*> EmployeeQuery::findByType(
 std::vector<const Employee*> EmployeeQuery::findBySalary(
     double salary, ComparisonOperator op
 ) const {
-    auto employees = _repo->employees();
+    auto employees = _repo->data();
     return from(employees).where(&Employee::baseSalary, salary, op).find();
 }
