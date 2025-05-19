cdefa9d144d17d1691d2af720d5fb9c8108d55d2
leducthanhig <leducthanh.ig@gmail.com>
2025-05-19 13:34:16 +0700

    chore: update and implement `MedicineUsage` class

--- src/models/MedicineUsage.cpp
+++ src/models/MedicineUsage.cpp
@@ -1,29 +1,73 @@
 #include "MedicineUsage.h"
 
-MedicineUsage::MedicineUsage() {
+MedicineUsage::MedicineUsage()
+    : _usedQuantity(), _price() {}
 
+MedicineUsage::MedicineUsage(
+    const std::string& id,
+    const std::string& patientId,
+    const std::string& doctorId,
+    const std::string& medicineId,
+    int usedQuantity,
+    double price,
+    const std::string& description
+)
+    : BillableComponent(id, "")
+    , _patientId(patientId)
+    , _doctorId(doctorId)
+    , _medicineId(medicineId)
+    , _usedQuantity(usedQuantity)
+    , _price(price)
+    , _description(description) {}
+
+std::string MedicineUsage::patientId() const {
+    return _patientId;
 }
 
-MedicineUsage::MedicineUsage(const Medicine& medicine, int dosage, std::pair<int, std::string> frequency, std::string instruction) {
-
+std::string MedicineUsage::doctorId() const {
+    return _doctorId;
 }
 
-MedicineUsage::~MedicineUsage() {
-
+std::string MedicineUsage::medicineId() const {
+    return _medicineId;
 }
 
-std::string MedicineUsage::getMedicineInfo() {
-
+int MedicineUsage::usedQuantity() const {
+    return _usedQuantity;
 }
 
-std::string MedicineUsage::getInstructions() {
-
+double MedicineUsage::price() const {
+    return _price;
 }
 
-void MedicineUsage::setMedicine(Medicine&) {
-
+std::string MedicineUsage::description() const {
+    return _description;
 }
 
-void MedicineUsage::setInstructions(std::string instruction) {
+void MedicineUsage::setPatientId(const std::string& patientId) {
+    _patientId = patientId;
+}
 
+void MedicineUsage::setDoctorId(const std::string& doctorId) {
+    _doctorId = doctorId;
+}
+
+void MedicineUsage::setMedicineId(const std::string& medicineId) {
+    _medicineId = medicineId;
+}
+
+void MedicineUsage::setUsedQuantity(int usedQuantity) {
+    _usedQuantity = usedQuantity;
+}
+
+void MedicineUsage::setPrice(double price) {
+    _price = price;
+}
+
+void MedicineUsage::setDescription(const std::string& description) {
+    _description = description;
+}
+
+double MedicineUsage::calculateFee() const {
+    return _price * _usedQuantity;
 }
