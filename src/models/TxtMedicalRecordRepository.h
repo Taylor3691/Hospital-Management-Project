71575c0b9cbef996f2ef35c59fe5746a8f3e7239
Taylor3691 <hoangngoc3691@gmail.com>
2025-05-24 21:55:05 +0700

    feat: add MedicalRecord repository class

---
+++ src/models/TxtMedicalRecordRepository.h
@@ -0,0 +1,30 @@
+#ifndef TXTMEDICALRECORDREPOSITORY_H_
+#define TXTMEDICALRECORDREPOSITORY_H_
+#include "../interfaces/IMedicalRecordRepository.h"
+
+class TxtMedicalRecordRepository : public IMedicalRecordRepository {
+private:
+    std::string _fileName;
+    char _delim;
+    std::vector<std::unique_ptr<MedicalRecord>> _records;
+
+public:
+    TxtMedicalRecordRepository();
+    TxtMedicalRecordRepository(const std::string& filename, char delim = '|');
+
+public:
+    const std::string& fileName() const;
+    char delim() const;
+    void setFileName(const std::string& fileName);
+    void setDelim(char delim);
+
+public:
+    void load() override;
+    void save() const override;
+    void add(std::unique_ptr<MedicalRecord> record) override;
+    void removeById(const std::string& id) override;
+    void update(const MedicalRecord& record) override;
+    std::vector<const MedicalRecord*> data() const override;
+};
+
+#endif // !TXTMEDICALRECORDREPOSITORY_H_
