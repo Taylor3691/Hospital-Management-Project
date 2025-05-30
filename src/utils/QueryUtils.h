759614bd25401a19e3b38f569886642e099b33c5
leducthanhig <leducthanh.ig@gmail.com>
2025-05-30 16:36:47 +0700

    chore(QueryUtils); remove unused functions

--- src/utils/QueryUtils.h
+++ src/utils/QueryUtils.h
@@ -3,53 +3,6 @@
 
 #include "QueryTypes.h"
 
-template<class ClassType, class ValueType>
-inline Getter<ClassType> makeGetter(ValueType(ClassType::* getter)() const) {
-    return [getter](const ClassType& obj) -> std::any {
-        return (obj.*getter)();
-        };
-}
-
-template<class ClassType, class ValueType>
-inline Getter<ClassType> makeGetter(const ValueType* (ClassType::* getter)() const) {
-    return [getter](const ClassType& obj) -> std::any {
-        return *(obj.*getter)();
-        };
-}
-
-template<class ClassType, class ValueType>
-inline Setter<ClassType> makeSetter(void(ClassType::* setter)(const ValueType&)) {
-    return [setter](const ClassType& obj, const ValueType& value) -> std::any {
-        return (obj.*setter)(value);
-        };
-}
-
-template<class Parent, class Child>
-inline Getter<Parent> compose(Getter<Parent> parent, Getter<Child> child) {
-    return [parent, child](const Parent& obj) -> std::any {
-        try {
-            auto parentValue = std::any_cast<Child>(parent(obj));
-            return child(parentValue);
-        }
-        catch (const std::bad_any_cast& e) {
-            throw std::runtime_error("Parentype mismatch in compose");
-        }
-        };
-}
-
-template<class Parent, class Child>
-inline Setter<Parent> compose(Getter<Parent> parent, Setter<Child> child) {
-    return [parent, child](const Parent& obj, const Child& value) -> std::any {
-        try {
-            auto parentValue = std::any_cast<Child>(parent(obj));
-            return child(parentValue, value);
-        }
-        catch (const std::bad_any_cast& e) {
-            throw std::runtime_error("Type mismatch in compose");
-        }
-        };
-}
-
 template<class ValueType>
 inline bool compare(const ValueType& value, const FilterCriteria& criteria) {
     if (auto ptr = std::get_if<ValueType>(&criteria.value)) {
